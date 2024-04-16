67b6b7eace6185a9d34fce03eb2104e41d2142dc
sachihiroo <saif3antar@gmail.com>
2024-04-16 07:29:53 +0100

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
 """
-This script will use the REST API to return information
-about a given employee's TODO list progress.
+This script fetches and displays information about completed tasks
 """
-import pandas as pd
 import requests
 import sys
+import csv
 
 
-def TODO_REQUESTS(ID):
+def _id(user_id):
     """
-    extend your Python script to export data in the CSV format
+    Fetches and displays information about completed tasks for a given user ID.
     """
-    todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
-    ).json()
-    user_info = requests.get(
-        f"https://jsonplaceholder.\
-typicode.com/users/{ID}"
-    ).json()
-    df = pd.DataFrame(todos)
-    df["username"] = user_info["username"]
-    df = df[["userId", "username", "completed", "title"]]
-    df.to_csv(f"{ID}.csv", index=False, quoting=1)
+    todo_url = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={user_id}"
+    )
+    user_name_url = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    )
+    """Convert response to JSON format"""
+    todo_dic = todo_url.json()
+    user_name_dic = user_name_url.json()
+
+    """Write task information to a CSV file"""
+    csv_filename = f"{user_id}.csv"
+    with open(csv_filename, mode="w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo_dic:
+            writer.writerow(
+                [(user_id), user_name_dic["username"],
+                 task["completed"], task["title"]]
+            )
 
 
 if __name__ == "__main__":
-    ID = sys.argv[1]
-    TODO_REQUESTS(ID)
+    """Retrieve the user ID from the command-line arguments"""
+    user_id = sys.argv[1]
+    _id(user_id)
